0ed92dee00e74bd89f93aaf6edd9ea50f0a6c432
SirPartyDyret <polyswing7@hotmail.com>
2017-11-13 15:28:35 +0100

    te

--- configs/routes.js
+++ configs/routes.js
@@ -1,54 +1,67 @@
-module.exports = [
-    {
+module.exports = [{
         method: 'GET',
         path: '/',
+        config: {
+            auth: {
+                mode: 'try',
+                strategy: 'session',
+            }
+        },
         handler: function (request, reply) {
-            reply.view('page');
+            const context = {
+                session: request.auth.credentials,
+            };
+
+            reply.view('page', {
+                context: context
+            });
         }
     },
 
     {
-        method: ['GET','POST'],
+        method: ['GET', 'POST'],
         path: '/dashboard',
         config: {
             auth: {
-              strategy: 'session',
-              mode: 'required'
+                strategy: 'session',
+                mode: 'required'
             },
-          },
+        },
         handler: function (request, reply) {
             const context = {
                 session: request.auth.credentials,
-              };
+            };
 
-              console.log(request.auth.credentials.username);
-        
-              reply.view('dashboard', {
-                title: 'Dashboard', 
-                context : context
-              });
+            console.log(request.auth.credentials.username);
+
+            reply.view('dashboard', {
+                title: 'Dashboard',
+                context: context
+            });
         }
     },
 
     {
         method: ['GET', 'POST'], // Must handle both GET and POST
-        path: '/login/{param*}',          // The callback endpoint registered with the provider
+        path: '/login/{param*}', // The callback endpoint registered with the provider
         config: {
             auth: {
                 mode: 'try',
                 strategy: 'spotify',
             },
             handler: function (request, reply) {
-    
+
                 if (!request.auth.isAuthenticated) {
                     return reply.redirect('/');
                 }
 
                 console.log(request.auth.credentials);
-    
+
                 const username = request.auth.credentials.profile.username
-                request.cookieAuth.set({ username });
-               
+                request.cookieAuth.set({
+                    username
+                });
+
                 return reply.redirect('/dashboard');
             }
         }
@@ -58,19 +71,19 @@ module.exports = [
         method: 'GET',
         path: '/logout',
         config: {
-          auth: {
-            mode: 'try',
-            strategy: 'session',
-          }
+            auth: {
+                mode: 'try',
+                strategy: 'session',
+            }
         },
         handler: function (request, reply) {
-          // Clear the cookie
-          request.cookieAuth.clear();
-          return reply.redirect('/');
+            // Clear the cookie
+            request.cookieAuth.clear();
+            return reply.redirect('/');
         }
-      },
+    },
 
-      {
+    {
         method: "GET",
         path: "/semantic/{param*}",
         config: {
